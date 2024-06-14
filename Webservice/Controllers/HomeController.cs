89a75c30de5bde974b1f1d83458400d0f08314c3
Zero-stack-0 <jangidshubham234@gmail.com>
2024-06-14 14:05:50 +0530

    created navbar and home page in progress

--- Webservice/Controllers/HomeController.cs
+++ Webservice/Controllers/HomeController.cs
@@ -1,24 +1,16 @@
-using System.Diagnostics;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
-using Webservice.Helper;
-using Webservice.Models;
 
 namespace Webservice.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly CookieUserDetailsHandler cookieUserDetailsHandler;
-
-    public HomeController(CookieUserDetailsHandler cookieUserDetailsHandler)
+    public HomeController()
     {
-        this.cookieUserDetailsHandler = cookieUserDetailsHandler;
+
     }
 
-    public async Task<IActionResult> Index()
+    public IActionResult Index()
     {
-        var claimsIdentity = User.Identity as ClaimsIdentity;
-        var requestor = await cookieUserDetailsHandler.GetUserDetail(claimsIdentity);
-        return View(requestor);
+        return View();
     }
 }
